ed05f9c71caa4dbff286932582ac5f6859c9d26d
spector-0110 <vatsaadityamishra19@gmail.com>
2025-06-19 16:23:02 +0530

    new

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Metadata } from "next/types";
+import { Geist } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import "@/styles/globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
